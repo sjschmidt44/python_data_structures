1b8afd8a7bbc832e1394cbca29624e24d5e7d062
Scott Schmidt <sjschmidt44@gmail.com>
2015-09-23 07:56:34 -0700

    Add basic structure and methods for linked list

---
+++ data_structures/linked_list.py
@@ -0,0 +1,29 @@
+class Node(object):
+    def __init__(self, val=None, pointer=None):
+        self.val = val
+        self.pointer = pointer
+
+
+class LinkedList(object):
+    def __init__(self, values=None, head=None):
+        self.head = head
+        self.length = 0
+
+
+def size(self):
+    pass
+
+def search(self):
+    pass
+
+def display(self):
+    pass
+
+def remove(self):
+    pass
+
+def insert(self):
+    pass
+
+def pop(self):
+    pass
